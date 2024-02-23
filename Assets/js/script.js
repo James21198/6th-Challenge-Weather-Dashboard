c4398f7e3e800cde4adc4680259b37d29b7d3047
James Rhodes <james21198@gmail.com>
2024-02-23 11:12:08 +0000

    HTML Structure Updated

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,8 +2,15 @@ const WEATHER_API_BASE_URL = 'https://api.openweathermap.org';
 const WEATHER_API_KEY = 'f23ee9deb4e1a7450f3157c44ed020e1';
 const MAX_DAILY_FORECAST = 5;
 
+var locationEl = document.getElementById('location');
+var searchButtonEl = document.getElementById('search');
+
+function onClickSearch() {
+    console.log('search button clicked');
+}
+
 // Lookup the location to get the Lat/Lon
-function doSomething() {
+function latLon() {
 
     var apiUrl = `${WEATHER_API_BASE_URL}/geo/1.0/direct?q=${search}&limit=5&appid=${WEATHER_API_KEY}`;
     fetch(apiUrl)
@@ -34,4 +41,6 @@ function doSomething() {
                 })
             })
 }
+
+searchButtonEl.addEventListener('click', onClickSearch);
         
