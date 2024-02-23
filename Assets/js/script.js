041467f203eac037ebfb1eed230fcfd8ccabb6dd
James Rhodes <james21198@gmail.com>
2024-02-23 12:04:26 +0000

    CSS and JS Files Updated First Draft

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,10 +7,18 @@ var searchButtonEl = document.getElementById('search');
 
 function onClickSearch() {
     console.log('search button clicked');
+
+    var locationName = locationEl.value;
+    if (locationName) {
+        locationLookup(locationName);
+    }
+    else {
+        console.log('no location name');
+    }
 }
 
 // Lookup the location to get the Lat/Lon
-function latLon() {
+function locationLookup() {
 
     var apiUrl = `${WEATHER_API_BASE_URL}/geo/1.0/direct?q=${search}&limit=5&appid=${WEATHER_API_KEY}`;
     fetch(apiUrl)
@@ -20,7 +28,7 @@ function latLon() {
             console.log(data);
 
             // Pick the First location from the results
-            //const location = data[0];
+            const location = data[0];
             var lat = data[0].lat;
             var lon = data[0].lon;
 
@@ -34,12 +42,38 @@ function latLon() {
                     console.log(data);
 
                     // Show the Current Weather Forecast
-                    
+                    displayCurrentWeather(weatherData);
 
                     // Show the 5 Day Weather Forecast
-                    
+                    displayWeatherForecast(weatherData.forecast);
                 })
             })
+}
+
+function displayCurrentWeather(weatherData) {
+    const currentWeather = weatherData.current;
+
+    document.getElementById('temp_value').textContent = `${currentWeather.temp}`;
+    document.getElementById('wind_value').textContent = `${currentWeather.wind_speed}MPH`;
+    document.getElementById('humid_value').textContent = `${currentWeather.temp}%`;
+    document.getElementById('uvi_value').textContent = `${currentWeather.temp}`;
+}
+
+function displayWeatherForecast(weatherData) {
+    for(let i = 0; i <MAX_DAILY_FORECAST; i++) {
+        const dailyForecast = dailyData[i];
+        console.log(dailyForecast);
+
+        const listItem = document.createElement ('li');
+        listItem.classList.add('forecast-item');
+        listItem.innerHTML = `
+            <div class="forecast-day">${day}</div>
+            <div class="forecast-temp">${temp}</div>
+            <div class="forecast-humidity">${humidity}</div>
+            <div class="forecast-wind">${wind}</div>
+        `;
+        forecastList.appendChild(listItem);
+    }
 }
 
 searchButtonEl.addEventListener('click', onClickSearch);
