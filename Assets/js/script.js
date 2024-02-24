9fc40252324418587eb4fb16f1f18169ad3913e4
James Rhodes <james21198@gmail.com>
2024-02-24 15:42:25 +0000

    JS File updated and amended

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,79 +2,41 @@ const WEATHER_API_BASE_URL = 'https://api.openweathermap.org';
 const WEATHER_API_KEY = 'f23ee9deb4e1a7450f3157c44ed020e1';
 const MAX_DAILY_FORECAST = 5;
 
-var locationEl = document.getElementById('location');
-var searchButtonEl = document.getElementById('search');
+const cityInput = document.querySelector (".city-input");
+const searchButton = document.querySelector (".search-btn");
 
-function onClickSearch() {
-    console.log('search button clicked');
+const getWeatherDetails = (cityName, lat, lon) => {
+    var apiUrl = `${WEATHER_API_BASE_URL}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${WEATHER_API_KEY}`;
 
-    var locationName = locationEl.value;
-    if (locationName) {
-        locationLookup(locationName);
-    }
-    else {
-        console.log('no location name');
-    }
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        const uniqueForcastDays = [];
+
+        const fiveDayForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if(!uniqueForcastDays.includes(forecastDate)) {
+                return uniqueForcastDays.push(forecastDate);
+            }
+        });
+
+        console.log(fiveDayForecast);
+    }).catch (() => {
+        alert("An error has occurred whilst getting the weather forecast!");
+    });
 }
 
-// Lookup the location to get the Lat/Lon
-function locationLookup(location) {
+const getCityCoordinates = () => {
+    const cityName = cityInput.value.trim();
+    if (!cityName) return;
+    
+    var geoapiUrl = `${WEATHER_API_BASE_URL}/geo/1.0/direct?q=${cityName}&limit=5&appid=${WEATHER_API_KEY}`;;
 
-    var apiUrl = `${WEATHER_API_BASE_URL}/geo/1.0/direct?q=${location}&limit=5&appid=${WEATHER_API_KEY}`;
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-
-            console.log(data);
-
-            // Pick the First location from the results
-            const location = data[0];
-            var lat = data[0].lat;
-            var lon = data[0].lon;
-
-            // Get the Weather for the cached location
-            var apiUrl = `${WEATHER_API_BASE_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${WEATHER_API_KEY}`;
-            
-            fetch(apiUrl)
-                .then(response => response.json())
-                .then(data => {
-
-                    console.log(data);
-
-                    // Show the Current Weather Forecast
-                    displayCurrentWeather(weatherData);
-
-                    // Show the 5 Day Weather Forecast
-                    displayWeatherForecast(weatherData.forecast);
-                })
-            })
+    fetch (geoapiUrl).then(res => res.json()).then(data => {
+        if(!data.length) return alert(`No coordinates found for ${cityName}`);
+        const {name, lat, lon} = data[0];
+        getWeatherDetails(name, lat, lon);
+    }).catch (() => {
+        alert("An error has occurred whilst trying to fetch your coordinates!");
+    });
 }
 
-function displayCurrentWeather(weatherData) {
-    const currentWeather = weatherData.current;
-
-    document.getElementById('temp_value').textContent = `${currentWeather.temp}°`;
-    document.getElementById('wind_value').textContent = `${currentWeather.wind_speed}MPH`;
-    document.getElementById('humid_value').textContent = `${currentWeather.humidity}%`;
-    document.getElementById('uvi_value').textContent = `${currentWeather.uvi}`;
-}
-
-function displayWeatherForecast(weatherData) {
-    for(let i = 0; i <MAX_DAILY_FORECAST; i++) {
-        const dailyForecast = dailyData[i];
-        console.log(dailyForecast);
-
-        const listItem = document.createElement ('li');
-        listItem.classList.add('forecast-item');
-        listItem.innerHTML = `
-            <div class="forecast-day">${day}</div>
-            <div class="forecast-temp">${temp}</div>
-            <div class="forecast-humidity">${humidity}</div>
-            <div class="forecast-wind">${wind}</div>
-        `;
-        forecastList.appendChild(listItem);
-    }
-}
-
-searchButtonEl.addEventListener('click', onClickSearch);
-        
+searchButton.addEventListener ("click", getCityCoordinates);
