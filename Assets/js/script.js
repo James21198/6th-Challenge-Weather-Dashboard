41929484f8d5caac92bc8c5d4f653b796b57200b
James Rhodes <james21198@gmail.com>
2024-02-25 14:24:39 +0000

    JS Current Date Updated

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,7 +11,7 @@ const createWheatherCard = (cityName, weatherItem, index) => {
     if (index === 0) {
         return `<div class="details">
                     <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-                    <h4>Temperature: ${(weatherItem.main.temp - 273.15.toFixed(0))}°C</h4>
+                    <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(0)}°C</h4>
                     <h4>Wind: ${weatherItem.wind.speed} MPH</h4>
                     <h4>Humidity: ${weatherItem.main.humidity}%</h4>
                 </div>
@@ -20,16 +20,16 @@ const createWheatherCard = (cityName, weatherItem, index) => {
                     <h4>${weatherItem.weather[0].description}</h4>
                 </div>`;
     } else {
-    return `<li class="card">
-                <h3>${weatherItem.dt_txt.split(" ")[0]}</h3>
-                <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
-                <h4>Temp: ${(weatherItem.main.temp - 273.15.toFixed(0))}°C</h4>
-                <h4>Wind: ${weatherItem.wind.speed} MPH</h4>
-                <h4>Humidity: ${weatherItem.main.humidity}%</h4>
-            </li>`;
+        return `<li class="card">
+                    <h3>${weatherItem.dt_txt.split(" ")[0]}</h3>
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
+                    <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(0)}°C</h4>
+                    <h4>Wind: ${weatherItem.wind.speed} MPH</h4>
+                    <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+                </li>`;
     }
 }
-
+console.log ();
 const getWeatherDetails = (cityName, lat, lon) => {
     var apiUrl = `${WEATHER_API_BASE_URL}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${WEATHER_API_KEY}`;
 
@@ -47,7 +47,7 @@ const getWeatherDetails = (cityName, lat, lon) => {
         currentWeatherDiv.innerHTML = "";
         weatherCardsDiv.innerHTML = "";
 
-        fiveDayForecast.forEach(weatherItem, index => {
+        fiveDayForecast.forEach((weatherItem, index) => {
             if (index === 0) {
                 currentWeatherDiv.insertAdjacentHTML("beforeend", createWheatherCard(cityName, weatherItem, index));
             } else {
