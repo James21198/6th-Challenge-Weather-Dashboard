ce4264f700e745fd37128bca80b3657e6b34f628
James Rhodes <james21198@gmail.com>
2024-02-26 18:46:12 +0000

    JS Recent Searches Added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,17 +1,53 @@
 const WEATHER_API_BASE_URL = 'https://api.openweathermap.org';
 const WEATHER_API_KEY = 'f23ee9deb4e1a7450f3157c44ed020e1';
-const MAX_DAILY_FORECAST = 5;
 
 const cityInput = document.querySelector (".city-input");
 const searchButton = document.querySelector (".search-btn");
 const weatherCardsDiv = document.querySelector (".weather-cards");
 const currentWeatherDiv = document.querySelector (".current-weather");
 
+const recentLocations = [];
+
+function loadRecentLocations() {
+    const storedLocations = JSON.parse(localStorage.getItem('recentLocations'));
+
+    if (storedLocations !== null) {
+        recentLocations.push(...storedLocations);
+
+        for (let i = 0; i < recentLocations.length; i++) {
+            var newLocation = document.createElement('div');
+            newLocation.classList.add('recent-location');
+            newLocation.textContent = recentLocations[i];
+            newLocation.addEventListener('click', onClickRecentLocation);
+
+            document.getElementById('recent-locations').appendChild(newLocation);
+        }
+    }
+}
+
+function onClickRecentLocation(event) {
+    console.log('clicked');
+
+    const location = event.target.textContent;
+    getCityCoordinates(location);
+}
+
+function saveRecentLocation(location) {
+    const index = recentLocations.indexOf(location);
+
+    if (index === -1) {
+        recentLocations.push(location);
+
+        localStorage.setItem('recentLocations', JSON.stringify(recentLocations));
+        loadRecentLocations();
+    }
+}
+
 const createWheatherCard = (cityName, weatherItem, index) => {
     if (index === 0) {
         return `<div class="details">
                     <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-                    <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(0)}°C</h4>
+                    <h4>Temperature: ${(weatherItem.main.temp - 27.15).toFixed(0)}°C</h4>
                     <h4>Wind: ${weatherItem.wind.speed} MPH</h4>
                     <h4>Humidity: ${weatherItem.main.humidity}%</h4>
                 </div>
@@ -23,13 +59,12 @@ const createWheatherCard = (cityName, weatherItem, index) => {
         return `<li class="card">
                     <h3>${weatherItem.dt_txt.split(" ")[0]}</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
-                    <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(0)}°C</h4>
+                    <h4>Temp: ${(weatherItem.main.temp - 27.15).toFixed(0)}°C</h4>
                     <h4>Wind: ${weatherItem.wind.speed} MPH</h4>
                     <h4>Humidity: ${weatherItem.main.humidity}%</h4>
                 </li>`;
     }
 }
-console.log ();
 const getWeatherDetails = (cityName, lat, lon) => {
     var apiUrl = `${WEATHER_API_BASE_URL}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${WEATHER_API_KEY}`;
 
@@ -69,9 +104,13 @@ const getCityCoordinates = () => {
         if(!data.length) return alert(`No coordinates found for ${cityName}`);
         const {name, lat, lon} = data[0];
         getWeatherDetails(name, lat, lon);
+        saveRecentLocation(location);
     }).catch (() => {
         alert("An error has occurred whilst trying to fetch your coordinates!");
     });
+
 }
+
+loadRecentLocations();
 
 searchButton.addEventListener ("click", getCityCoordinates);
