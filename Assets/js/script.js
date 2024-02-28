059c852033ba8b0a0f59080b06e439aa707c9651
James Rhodes <james21198@gmail.com>
2024-02-28 09:08:43 +0000

    JS Files Comment added and Readme file updated

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,7 +7,7 @@ const weatherCardsDiv = document.querySelector (".weather-cards");
 const currentWeatherDiv = document.querySelector (".current-weather");
 
 const recentLocations = [];
-
+// This function ensures that when the page is reloaded the previous search items stay visible on the page
 function loadRecentLocations() {
     
     const recentLocations = JSON.parse(localStorage.getItem('recentLocations'));
@@ -25,14 +25,14 @@ function loadRecentLocations() {
         }
     }
 }
-
+// This function is ensuring that once the recent search is clicked that it then loads the weather information for that city
 function onClickRecentLocation(event) {
-    console.log('clicked');
-
     const searchLocation = event.target.textContent;
     getCityCoordinates(searchLocation);
 }
 
+// This function is saving the users current search into the local storage and is also ensuring that if the same
+// city is entered twice that it doesnt add multiple lines for that city
 function saveRecentLocation(searchLocation) {
     const recentLocations = JSON.parse(localStorage.getItem('recentLocations'))||[]
     const index = recentLocations.indexOf(searchLocation);
@@ -47,6 +47,7 @@ function saveRecentLocation(searchLocation) {
 
 const createWheatherCard = (cityName, weatherItem, index) => {
     if (index === 0) {
+        // this is adding the weather information to the current day weather card
         return `<div class="details">
                     <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
                     <h4>Temperature: ${(weatherItem.main.temp - 27.15).toFixed(0)}°C</h4>
@@ -58,6 +59,7 @@ const createWheatherCard = (cityName, weatherItem, index) => {
                     <h4>${weatherItem.weather[0].description}</h4>
                 </div>`;
     } else {
+        // This is addding the weather inforamtion to the five day forecasts
         return `<li class="card">
                     <h3>${weatherItem.dt_txt.split(" ")[0]}</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
@@ -71,20 +73,19 @@ const getWeatherDetails = (cityName, lat, lon) => {
     var apiUrl = `${WEATHER_API_BASE_URL}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${WEATHER_API_KEY}`;
 
     fetch(apiUrl).then(res => res.json()).then(data => {
-        console.log(data);
         const uniqueForcastDays = [];
-
+        // This filters the forecast to ensure that you are only getting one forecast per day
         const fiveDayForecast = data.list.filter(forecast => {
             const forecastDate = new Date(forecast.dt_txt).getDate();
             if(!uniqueForcastDays.includes(forecastDate)) {
                 return uniqueForcastDays.push(forecastDate);
             }
         });
-        console.log(uniqueForcastDays);
+        // This is clearing the previous weather data
         cityInput.value = "";
         currentWeatherDiv.innerHTML = "";
         weatherCardsDiv.innerHTML = "";
-
+        // This is creating the weather cards and is then adding them to the DOM
         fiveDayForecast.forEach((weatherItem, index) => {
             if (index === 0) {
                 currentWeatherDiv.insertAdjacentHTML("beforeend", createWheatherCard(cityName, weatherItem, index));
@@ -98,7 +99,9 @@ const getWeatherDetails = (cityName, lat, lon) => {
 }
 
 function onClickSearch (event) {
+    // This gets the users entered city name and removes any extra spaces
     const cityName = cityInput.value.trim();
+    // This will return to the original state if the city name is empty
     if (!cityName) return;
     getCityCoordinates(cityName);
 }
@@ -106,7 +109,7 @@ function onClickSearch (event) {
 const getCityCoordinates = (cityName) => {
     
     var geoapiUrl = `${WEATHER_API_BASE_URL}/geo/1.0/direct?q=${cityName}&limit=5&appid=${WEATHER_API_KEY}`;;
-
+    // Get entered city coordinates from the API Response, for example latitude, longitude and the name of the city
     fetch (geoapiUrl).then(res => res.json()).then(data => {
         if(!data.length) return alert(`No coordinates found for ${cityName}`);
         const {name, lat, lon} = data[0];
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 6th-Challenge-Weather-Dashboard
+
+## What was my project objective and what have i accomplished?
+In this project i was tasked with creating a fully functioning weather dashboard which i have sucessfully done by ensuring that when a city name for example, London is entered into the text box and the search button is pressed that the current weather as well as a five day forecast is then presented on the page. I have also ensured that each day that is presented on the weather dashboard shows the temperature, wind speed, the humidity and that an icon shows what the weather prediction is. For example if it is expected to rain an icon of a raining cloud is displayed.
+
+I was also tasked with ensuring that once a city name is entered that it is then added to a recent search area so that the user is able to quickly click back on their previous search if they need to do so which i have sucessfully been able to do by ensuring that when a previous search is clicked the user is then presented with all of the weather information for that city.
+
+
+![alt text](./Assets/images/Weather-dashboard.png)
+
